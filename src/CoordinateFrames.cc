832d7a3055aa5a26bf9971a07b72c985fba6a227
Knoblauchpilze <prolific-programmer@tuta.com>
2020-08-29 23:30:51 +0200

    Almost not working implementation of translation (as the tiles are isometric it ruins the way we're usually doing translations)

--- src/CoordinateFrames.cc
+++ src/CoordinateFrames.cc
@@ -17,24 +17,24 @@ namespace new_frontiers {
     m_tScaled(m_ts),
 
     m_translationOrigin(),
-    m_cachedPOrigin()
+    m_cachedCOrigin()
   {
     setService("coordinate");
 
     // Update the tiles scale based on the viewport.
     updateTileScale();
-
-    log("cViewport: " + toString(m_cViewport.dims), utils::Level::Info);
-    log("pViewport: " + toString(m_pViewport.dims), utils::Level::Info);
-    log("real size: " + toString(m_ts), utils::Level::Info);
-    log("on screen: " + toString(m_scale), utils::Level::Info);
-    log("scaling  : " + toString(m_tScaled), utils::Level::Info);
   }
 
   void
   CoordinateFrames::updateTileScale() {
     m_tScaled = m_pViewport.dims / m_cViewport.dims;
     m_scale = m_tScaled / m_ts;
+
+    log("cViewport: " + toString(m_cViewport.dims), utils::Level::Info);
+    log("pViewport: " + toString(m_pViewport.dims), utils::Level::Info);
+    log("real size: " + toString(m_ts), utils::Level::Info);
+    log("on screen: " + toString(m_scale), utils::Level::Info);
+    log("scaling  : " + toString(m_tScaled), utils::Level::Info);
   }
 
 }
