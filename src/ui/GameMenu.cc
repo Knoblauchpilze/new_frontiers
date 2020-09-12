02d6c8475c1f942f10191610b180288ba1f3a4f0
Knoblauchpilze <prolific-programmer@tuta.com>
2020-09-12 16:01:56 +0200

    Updated background color for menus

--- src/ui/GameMenu.cc
+++ src/ui/GameMenu.cc
@@ -41,15 +41,17 @@ namespace new_frontiers {
     // Register building categories.
     olc::vi2d bDims(40, 40);
     MenuShPtr creature = std::make_shared<Menu>(olc::vi2d(), bDims, "creature_menu");
-    // creature->setBackground(newImageBackground("data/img/menu/devil_mask.png"));
+    creature->setBackground(newColoredBackground(olc::Pixel(59, 71, 17)));
     creature->setContent(newImageContent("data/img/menu/devil_mask.png", bDims));
     addMenu(creature);
 
     MenuShPtr housing = std::make_shared<Menu>(olc::vi2d(0, 5 + bDims.y), bDims, "housing_menu");
+    housing->setBackground(newColoredBackground(olc::Pixel(30, 66, 75)));
     housing->setContent(newImageContent("data/img/menu/house.png", bDims));
     addMenu(housing);
 
     MenuShPtr spells = std::make_shared<Menu>(olc::vi2d(0, 5 + 2 * bDims.y), bDims, "housing_menu");
+    spells->setBackground(newColoredBackground(olc::Pixel(30, 66, 42)));
     spells->setContent(newImageContent("data/img/menu/spells.png", bDims));
     addMenu(spells);
 
