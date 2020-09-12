1c6c0e4dba6e34d12d156f751582f5fc6b9a3e1c
Knoblauchpilze <maxime.rigaud.perso@gmail.com>
2020-09-12 11:35:02 +0200

    Provided working implementation for the case where both an icon and a text are provided

--- src/ui/Menu.cc
+++ src/ui/Menu.cc
@@ -16,8 +16,7 @@ namespace new_frontiers {
 
     m_bg(newColoredBackground(olc::Pixel(255, 255, 255, 0))),
     m_bgSprite(nullptr),
-    // m_content(newTextContent("default menu", Alignment::Left)),
-    m_content(newMenuContent("default menu", "data/img/menu/minimap.png", olc::vi2d(300, 100))),
+    m_content(newTextContent("default menu", Alignment::Left)),
     m_mcSprite(nullptr),
 
     m_layout(layout),
@@ -149,24 +148,22 @@ namespace new_frontiers {
     // if needed. We assume the content will always
     // be centered along the perpendicular axis for
     // this menu.
-    switch (m_layout) {
-      case Layout::Vertical:
-        break;
-      case Layout::Horizontal:
-      default:
-        break;
-    }
-
     // Depending on whether we need to display both
     // an image or a text and in which order layout
     // in the menu will change.
-    olc::vi2d ap = absolutePosition();
+    // Also, in case there's no text nor sprite to
+    // display we can return right now.
+    if (m_content.text == "" && m_mcSprite == nullptr) {
+      return;
+    }
 
+    olc::vi2d ap = absolutePosition();
 
     if (m_content.text != "" && m_mcSprite == nullptr) {
       olc::vi2d ts = pge->GetTextSize(m_content.text);
 
       olc::vi2d p;
+
       switch (m_content.align) {
         case Alignment::Center:
           p = olc::vi2d(
@@ -194,14 +191,16 @@ namespace new_frontiers {
         m_content.text,
         olc::MAGENTA
       );
+
+      return;
     }
 
     if (m_content.text == "" && m_mcSprite != nullptr) {
       // Center the image if it is the only element
       // to display.
       olc::vi2d p(
-        std::max(static_cast<int>(ap.x + m_size.x / 2.0f - m_content.size.x / 2.0f), 0),
-        std::max(static_cast<int>(ap.y + m_size.y / 2.0f - m_content.size.y / 2.0f), 0)
+        static_cast<int>(ap.x + m_size.x / 2.0f - m_content.size.x / 2.0f),
+        static_cast<int>(ap.y + m_size.y / 2.0f - m_content.size.y / 2.0f)
       );
 
       olc::vi2d ss(m_mcSprite->sprite->width, m_mcSprite->sprite->height);
@@ -214,9 +213,115 @@ namespace new_frontiers {
         ss,
         s
       );
+
+      return;
     }
 
-    // Both text and icon should be displayed.
+    // Both text and icon should be displayed: the
+    // order is specified in the content description.
+    olc::vi2d ts = pge->GetTextSize(m_content.text);
+    olc::vi2d cs = ts + m_content.size;
+
+    olc::vi2d tp;
+    olc::vi2d sp;
+
+    olc::vi2d ss(m_mcSprite->sprite->width, m_mcSprite->sprite->height);
+    olc::vf2d s(1.0f * m_content.size.x / ss.x, 1.0f * m_content.size.y / ss.y);
+
+    switch (m_content.order) {
+      case Ordering::TextFirst:
+        switch (m_content.align) {
+          case Alignment::Center:
+            tp = olc::vi2d(
+              static_cast<int>(ap.x + (m_size.x - cs.x) / 2.0f),
+              static_cast<int>(ap.y + (m_size.y - ts.y) / 2.0f)
+            );
+
+            sp = olc::vi2d(
+              static_cast<int>(ap.x + (m_size.x - cs.x) / 2.0f + ts.x),
+              static_cast<int>(ap.y + m_size.y / 2.0f - m_content.size.y / 2.0f)
+            );
+            break;
+          case Alignment::Right:
+            tp = olc::vi2d(
+              ap.x + m_size.x - cs.x,
+              static_cast<int>(ap.y + (m_size.y - ts.y) / 2.0f)
+            );
+
+            sp = olc::vi2d(
+              ap.x + m_size.x - m_content.size.x,
+              static_cast<int>(ap.y + m_size.y / 2.0f - m_content.size.y / 2.0f)
+            );
+            break;
+          case Alignment::Left:
+          default:
+            tp = olc::vi2d(
+              ap.x,
+              static_cast<int>(ap.y + (m_size.y - ts.y) / 2.0f)
+            );
+
+            sp = olc::vi2d(
+              ap.x + ts.x,
+              static_cast<int>(ap.y + m_size.y / 2.0f - m_content.size.y / 2.0f)
+            );
+            break;
+        }
+        break;
+      case Ordering::ImageFirst:
+      default:
+        switch (m_content.align) {
+          case Alignment::Center:
+            tp = olc::vi2d(
+              static_cast<int>(ap.x + (m_size.x - cs.x) / 2.0f + m_content.size.x),
+              static_cast<int>(ap.y + (m_size.y - ts.y) / 2.0f)
+            );
+
+            sp = olc::vi2d(
+              static_cast<int>(ap.x + (m_size.x - cs.x) / 2.0f),
+              static_cast<int>(ap.y + m_size.y / 2.0f - m_content.size.y / 2.0f)
+            );
+            break;
+          case Alignment::Right:
+            tp = olc::vi2d(
+              ap.x + m_size.x - ts.x,
+              static_cast<int>(ap.y + (m_size.y - ts.y) / 2.0f)
+            );
+
+            sp = olc::vi2d(
+              ap.x + m_size.x - cs.x,
+              static_cast<int>(ap.y + m_size.y / 2.0f - m_content.size.y / 2.0f)
+            );
+            break;
+          case Alignment::Left:
+          default:
+            tp = olc::vi2d(
+              ap.x + m_content.size.x,
+              static_cast<int>(ap.y + (m_size.y - ts.y) / 2.0f)
+            );
+
+            sp = olc::vi2d(
+              ap.x,
+              static_cast<int>(ap.y + m_size.y / 2.0f - m_content.size.y / 2.0f)
+            );
+            break;
+        }
+        break;
+    }
+
+    // Draw both the text and the image.
+    pge->DrawStringDecal(
+      tp,
+      m_content.text,
+      olc::MAGENTA
+    );
+
+    pge->DrawPartialDecal(
+      sp,
+      m_mcSprite,
+      olc::vi2d(),
+      ss,
+      s
+    );
   }
 
   void
