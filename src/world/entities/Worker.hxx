bbb4a4ed81cb04db4653538645a431e8a628e782
Knoblauchpilze <maxime.rigaud.perso@gmail.com>
2020-10-05 19:51:19 +0200

    Provided implementation for the location of elements in the 'Locator' class

--- src/world/entities/Worker.hxx
+++ src/world/entities/Worker.hxx
@@ -7,13 +7,6 @@ namespace new_frontiers {
 
   inline
   bool
-  Worker::collect(StepInfo& /*info*/, float& /*x*/, float& /*y*/) {
-    // TODO: Implement this.
-    return false;
-  }
-
-  inline
-  bool
   Worker::getBack(StepInfo& /*info*/, float& /*x*/, float& /*y*/) {
     // TODO: Implement this.
     return false;
