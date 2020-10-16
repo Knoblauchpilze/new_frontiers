ab99d5f022b3c3724c099d9dd81f4913cae07575
Knoblauchpilze <prolific-programmer@tuta.com>
2020-10-16 09:12:05 +0200

    Provided first implementation for Colony

---
+++ src/world/colonies/Colony.hxx
@@ -0,0 +1,15 @@
+#ifndef    COLONY_HXX
+# define   COLONY_HXX
+
+# include "Colony.hh"
+
+namespace new_frontiers {
+
+  inline
+  Colony::~Colony() {
+    // Nothing to do yet.
+  }
+
+}
+
+#endif    /* COLONY_HXX */
