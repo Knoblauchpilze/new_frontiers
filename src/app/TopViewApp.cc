8c61bb1a9ffbdd0a9fffc983bf18910cc8894ec5
Knoblauchpilze <prolific-programmer@tuta.com>
2020-10-18 11:04:41 +0200

    Propagated colonies display code to TopViewApp

--- src/app/TopViewApp.cc
+++ src/app/TopViewApp.cc
@@ -185,8 +185,75 @@ namespace new_frontiers {
     SetPixelMode(olc::Pixel::ALPHA);
     Clear(olc::Pixel(255, 255, 255, ALPHA_TRANSPARENT));
 
-    if (res.ui != nullptr) {
-      res.ui->render(this);
+    // Draw the colonies. We first need to draw the
+    // transparent background and then draw each
+    // colony's information.
+    olc::vi2d s;
+    int cOffset = 3;
+    int icOffset = 2;
+    olc::Pixel bg(255, 255, 255, ALPHA_SEMI_OPAQUE);
+    int tpSize = 20;
+
+    for (int id = 0 ; id < res.loc->coloniesCount() ; ++id) {
+      world::Colony c = res.loc->colony(id);
+      olc::vi2d idS = GetTextSize(c.id.toString());
+
+      olc::vi2d idFocus = GetTextSize(world::focusToString(c.focus));
+
+      s.x = std::max(s.x, idS.x + icOffset + tpSize + icOffset + idFocus.x);
+      s.y += (idS.y + cOffset);
+    }
+
+    FillRectDecal(olc::vf2d(), s, bg);
+
+    olc::vf2d p;
+    for (int id = 0 ; id < res.loc->coloniesCount() ; ++id) {
+      world::Colony c = res.loc->colony(id);
+
+      // Draw the colony's identifier.
+      DrawStringDecal(p, c.id.toString());
+
+      olc::vi2d idS = GetTextSize(c.id.toString());
+
+      // Draw the bar corresponding to the resource
+      // budget.
+      olc::vf2d tp(idS.x + icOffset, p.y);
+      olc::Pixel tint = redToGreenGradient(c.ratio, ALPHA_OPAQUE);
+      olc::Pixel emptyTint(
+        static_cast<int>(tint.r * 0.5f),
+        static_cast<int>(tint.g * 0.5f),
+        static_cast<int>(tint.b * 0.5f),
+        tint.a
+      );
+
+      FillRectDecal(tp, olc::vf2d(c.ratio * tpSize, idS.y), tint);
+      FillRectDecal(
+        olc::vf2d(tp.x + c.ratio * tpSize, tp.y),
+        olc::vf2d((1.0f - c.ratio) * tpSize, idS.y),
+        emptyTint
+      );
+
+      // Draw the current focus of this colony rendered
+      // as a string.
+      olc::vf2d tf(idS.x + tpSize + icOffset, p.y);
+      olc::Pixel fTint = olc::WHITE;
+      switch (c.focus) {
+        case colony::Priority::Consolidation:
+          fTint = olc::GREEN;
+          break;
+        case colony::Priority::Expansion:
+          fTint = olc::BLUE;
+          break;
+        case colony::Priority::War:
+          fTint = olc::RED;
+          break;
+        default:
+          break;
+      }
+
+      DrawStringDecal(tf, world::focusToString(c.focus), fTint);
+
+      p.y += (idS.y + cOffset);
     }
 
     SetPixelMode(olc::Pixel::NORMAL);
@@ -249,10 +316,11 @@ namespace new_frontiers {
     olc::vf2d it;
     olc::vi2d mtp = res.cf.pixelCoordsToTiles(mp, &it);
 
+    int h = GetDrawTargetHeight();
     int dOffset = 15;
-    DrawString(olc::vi2d(0, 0 * dOffset), "Mouse coords      : " + toString(mp), olc::CYAN);
-    DrawString(olc::vi2d(0, 1 * dOffset), "World cell coords : " + toString(mtp), olc::CYAN);
-    DrawString(olc::vi2d(0, 2 * dOffset), "Intra cell        : " + toString(it), olc::CYAN);
+    DrawString(olc::vi2d(0, h - 3 * dOffset), "Mouse coords      : " + toString(mp), olc::CYAN);
+    DrawString(olc::vi2d(0, h - 2 * dOffset), "World cell coords : " + toString(mtp), olc::CYAN);
+    DrawString(olc::vi2d(0, h - 1 * dOffset), "Intra cell        : " + toString(it), olc::CYAN);
 
     SetPixelMode(olc::Pixel::NORMAL);
   }
