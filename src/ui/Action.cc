af88a615ec415e4578eaa55e9baa29b92cd27b2d
Knoblauchpilze <maxime.rigaud.perso@gmail.com>
2020-10-25 14:04:05 +0100

    Added implementation for menus to produce action that are then applied onto the world by the main app

---
+++ src/ui/Action.cc
@@ -0,0 +1,6 @@
+
+# include "Action.hh"
+
+namespace new_frontiers {
+
+}
---
+++ src/ui/Action.hh
@@ -0,0 +1,48 @@
+#ifndef    ACTION_HH
+# define   ACTION_HH
+
+# include <memory>
+# include <core_utils/CoreObject.hh>
+# include "../world/World.hh"
+
+namespace new_frontiers {
+
+  class Action: public utils::CoreObject {
+    public:
+
+      /**
+       * @brief - Desctruction of the object.
+       */
+      virtual ~Action();
+
+      /**
+       * @brief - Interface method allowing to perform an
+       *          action on the input world. It aims at
+       *          providing a common interface allowing
+       *          menus to trigger some real changes in the
+       *          world.
+       *          Inheriting classes should redefine this
+       *          method based on their effect on the world.
+       * @param w - the world onto which the action should
+       *            be applied.
+       */
+      virtual void
+      apply(World& w) const = 0;
+
+    protected:
+
+      /**
+       * @brief - Create a new empty action.
+       * @param name - the name of the action (for logging purposes).
+       */
+      Action(const std::string& name);
+
+    private:
+  };
+
+  using ActionShPtr = std::shared_ptr<Action>;
+}
+
+# include "Action.hxx"
+
+#endif    /* ACTION_HH */
