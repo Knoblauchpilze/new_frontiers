b25cd3120e96722b5c70cf8dad74189a881cda00
Knoblauchpilze <maxime.rigaud.perso@gmail.com>
2020-11-11 12:35:39 +0100

    Started to refactor the 'generatePathTo' method in order to use the A* algorithm instead of obstacle tracing

--- src/world/entities/Path.cc
+++ src/world/entities/Path.cc
@@ -7,224 +7,53 @@
 
 namespace {
 
-  enum class Axis {
-    X,
-    Y
+  /**
+   * @brief- Convenience structure allowing to regroup the
+   *         information and processes needed to perform
+   *         the A* algorithm.
+   */
+  struct AStar {
+    new_frontiers::Point start;
+    new_frontiers::Point end;
+
+    const new_frontiers::Locator& loc;
+
+    /**
+     * @brief - Used to generate the path from the start
+     *          position to the end and return the path
+     *          that was built in the output argument.
+     *          In case no path can be found, the return
+     *          value will indicate so.
+     * @param path - the path generated to reach the two
+     *               end points of this finder.
+     * @return - `true` if a path could be find.
+     */
+    bool
+    findPath(std::vector<new_frontiers::Point>& path) const noexcept;
   };
-
-  enum class Direction {
-    None,
-    PositiveX,
-    NegativeX,
-    PositiveY,
-    NegativeY
-  };
-
-  // void
-  // nextCell(float x, float y, float xDir, float yDir, float& xN, float& yN) {
-  //   // We need to compute the first point on the boundary
-  //   // of the cell which will be intersected by the path
-  //   // defined by `[(x, y); (xDir, yDir)]`.
-  //   // We will handle the trivial case of no direction in
-  //   // a first time.
-  //   float xi = std::floor(x), yi = std::floor(y);
-
-  //   float l = std::sqrt(xDir * xDir + yDir * yDir);
-  //   if (l < 0.0001f) {
-  //     xN = x;
-  //     yN = y;
-
-  //     return;
-  //   }
-
-  //   // Compute how many times the `xDir` and `yDir` can
-  //   // fit respectively in the `x` and `y` direction.
-  //   float dx = (xDir < 0.0f ? x - xi : std::ceil(x) - x);
-  //   float dy = (yDir < 0.0f ? y - yi : std::ceil(y) - y);
-
-  //   float cx = dx / xDir, cy = dy / yDir;
-
-  //   // Whichever of `cx` or `cy` is smaller means that
-  //   // it will be reached first.
-  //   if (cx < cy) {
-  //     xN = xi + std::copysign(1.0f, xDir);
-  //     yN = yi;
-  //   }
-  //   else if (cy < cx) {
-  //     xN = xi;
-  //     yN = yi + std::copysign(1.0f, yDir);
-  //   }
-  //   else {
-  //     xN = xi + std::copysign(1.0f, xDir);
-  //     yN = yi + std::copysign(1.0f, yDir);
-  //   }
-  // }
 
   /**
-   * @brief - Compute the offset to apply to the position we're
-   *          considering the input direction and generate the
-   *          next position to consider.
-   * @param p - the current position to which the offset will
-   *            be applied.
-   * @param xDir - the abscissa of the direction we're using.
-   * @param yDir - the ordinate of the direction we're using.
-   * @param dir - the current direction of travel. Will be used
-   *              to update the followed direction.
-   * @param loc - a locator to help take decisions and provide
-   *              a next unobstructed position.
-   * @return - a set of two coordinates representing the next
-   *           position to consider.
+   * @brief - Creates a new object allowing to perform the
+   *          A* algorithm.
+   * @param s - the starting position.
+   * @param e - the end position.
+   * @param loc - the locator to use to retrieve information
+   *              about the world.
+   * @return - the A* structure.
    */
-  new_frontiers::Point
-  computeNextPosition(new_frontiers::Point p,
-                      float xDir,
-                      float yDir,
-                      Direction& dir,
-                      const new_frontiers::Locator& loc)
+  AStar
+  newAStarPathFinder(const new_frontiers::Point& s,
+                     const new_frontiers::Point& e,
+                     const new_frontiers::Locator& loc)
   {
-    // Compute the parallel direction (i.e. the direction
-    // with the least amount of momentum).
-    float aXDir = std::abs(xDir);
-    float aYDir = std::abs(yDir);
+    return AStar{s, e, loc};
+  }
 
-    Axis dPara = (aXDir > aYDir ? Axis::Y : Axis::X);
-
-    // The direction of the offset will be the same as
-    // it was before (if any value is assigned) unless
-    // the direction has switched to the perpendicular
-    // direction of the previous one. We also have to
-    // take into account cases where the direction can
-    // be resumed to something closer to the target if
-    // the path is not unobstructed anymore.
-    // Note that by default we use a positive direction
-    // to explore.
-
-    // So first, determine the direction as it would
-    // be in case we were to start anew.
-    Axis d = dPara;
-    float sPara = 1.0f;
-
-    switch (dir) {
-      case Direction::PositiveX:
-        d = Axis::X;
-        sPara = 1.0f;
-        break;
-      case Direction::PositiveY:
-        d = Axis::Y;
-        sPara = 1.0f;
-        break;
-      case Direction::NegativeX:
-        d = Axis::X;
-        sPara = -1.0f;
-        break;
-      case Direction::NegativeY:
-        d = Axis::Y;
-        sPara = -1.0f;
-        break;
-      case Direction::None:
-      default:
-        // Only `dir` is left to initialize. Choose
-        // the positive direction by default.
-        if (d == Axis::X) {
-          sPara = std::copysign(1.0f, xDir);
-          dir = (sPara > 0.0f ? Direction::PositiveX : Direction::NegativeX);
-        }
-        else {
-          sPara = std::copysign(1.0f, yDir);
-          dir = (sPara > 0.0f ? Direction::PositiveY : Direction::NegativeY);
-        }
-        break;
-    }
-
-    std::cout << "[PATH] point: " << p.x << "x" << p.y
-              << " d: " << xDir << "x" << yDir
-              << ", dPara: " << (d == Axis::X ? "x" : "y")
-              << ", sign: " << sPara
-              << ", computed dPara: " << (dPara == Axis::X ? "x" : "y")
-              << ", dir is " << (dir == Direction::None ? "none" : (dir == Direction::PositiveX ? "pos x" : (dir == Direction::PositiveY ? "pos y" : (dir == Direction::NegativeX ? "neg x" : "neg y"))))
-              << std::endl;
-
-    // Make sure that we continue in the same
-    // direction as we were except if the new
-    // direction is perpendicular.
-    if ((dir == Direction::PositiveX || dir == Direction::NegativeX) && dPara == Axis::Y) {
-      sPara = std::copysign(1.0f, yDir);
-      dir = (sPara > 0.0f ? Direction::PositiveY : Direction::NegativeY);
-    }
-    else if ((dir == Direction::PositiveY || dir == Direction::NegativeY) && dPara == Axis::X) {
-      sPara = std::copysign(1.0f, xDir);
-      dir = (sPara > 0.0f ? Direction::PositiveX : Direction::NegativeX);
-    }
-
-    new_frontiers::Point op = p;
-    switch (d) {
-      case Axis::X:
-        op.x += sPara;
-        break;
-      case Axis::Y:
-      default:
-        op.y += sPara;
-        break;
-    }
-
-    // Make sure that the chosen cell is unobstructed:
-    // if it is the case we will have to rotate so we
-    // continue to trace the obstacle.
-    if (!loc.obstructed(op)) {
-      return op;
-    }
-
-    // Rotate in order to follow the obstacle's border.
-    Direction rd = dir;
-    switch (dir) {
-      case Direction::PositiveX:
-        rd = Direction::NegativeY;
-        d = Axis::Y;
-        sPara = -1.0f;
-        break;
-      case Direction::PositiveY:
-        rd = Direction::PositiveX;
-        d = Axis::X;
-        sPara = 1.0f;
-        break;
-      case Direction::NegativeX:
-        rd = Direction::PositiveY;
-        d = Axis::Y;
-        sPara = 1.0f;
-        break;
-      case Direction::NegativeY:
-        rd = Direction::NegativeX;
-        d = Axis::X;
-        sPara = -1.0f;
-        break;
-      case Direction::None:
-      default:
-        // Should never happen.
-        break;
-    }
-
-    new_frontiers::Point ops = op;
-
-    op = p;
-    switch (d) {
-      case Axis::X:
-        op.x += sPara;
-        break;
-      case Axis::Y:
-      default:
-        op.y += sPara;
-        break;
-    }
-
-    std::cout << "[PATH] point: " << p.x << "x" << p.y
-              << " d: " << xDir << "x" << yDir
-              << ", led to " << ops.x << "x" << ops.y
-              << " but was obstructed so chose "
-              << op.x << "x" << op.y
-              << " (dir: " << (rd == Direction::None ? "none" : (rd == Direction::PositiveX ? "pos x" : (rd == Direction::PositiveY ? "pos y" : (rd == Direction::NegativeX ? "neg x" : "neg y")))) << ")"
-              << std::endl;
-
-    return op;
+  bool
+  AStar::findPath(std::vector<new_frontiers::Point>& path) const noexcept {
+    // TODO: Implement this.
+    path.clear();
+    return false;
   }
 
 }
@@ -305,7 +134,6 @@ namespace new_frontiers {
       }
 
       float xDir, yDir, d;
-      Point obsP;
       info.toDirection(s, p, xDir, yDir, d);
 
       // Detect trivial case where the target is a
@@ -316,135 +144,56 @@ namespace new_frontiers {
         return false;
       }
 
-      // TODO: Case where we reach a position that
-      // was already explored. This is what prevents
-      // the code from working right now.
+      // First, try to find a straight path to the
+      // target: if this is possible it's cool.
+      Point obsP;
+      std::vector<Point> iPoints;
 
-      // Process the path: we want to keep finding
-      // intermediate positions until we can find
-      // an unobstructed path to the target. This
-      // could very well be right away in case the
-      // path is directly visible.
-      std::vector<Point> points;
+      bool obs = info.frustum->obstructed(s, xDir, yDir, d, iPoints, &obsP);
+      bool obsWithinTarget = obs && (std::abs(obsP.x - p.x) < 1.0f && std::abs(obsP.y - p.y) < 1.0f);
+
+      if (!obs || (obsWithinTarget && ignoreTargetObstruction)) {
+        // There is an obstruction but it is within
+        // the target and we ignore it so we could
+        // find a path.
+        for (unsigned id = 0u ; id < iPoints.size() - 1 ; ++id) {
+          cPoints.push_back(iPoints[id]);
+        }
+
+        add(p);
+
+        return true;
+      }
+
+      // Note that at this point we know:
+      //  - that the target is not obstructed or we
+      //    ignore it.
+      //  - there is no straight path to the target.
+      // So with this information in mind, we will
+      // try to run a A* algorithm. We thought about
+      // relying on a technique called the `obstacle
+      // tracing` algorithm but it appears that it's
+      // not really easy to implement and we prefer
+      // to choose safety. It is not exactly what we
+      // desired (the A*) as it means that each mob
+      // has indeed infinite vision for now but
+      // that's it.
+      // TODO: Maybe include a sort of distance
+      // in the A* which will return `false` and
+      // the entity would then go back to real
+      // random wandering.
+      AStar alg = newAStarPathFinder(s, p, *info.frustum);
       std::vector<Point> steps;
 
-      bool pathNotClear = true;
-      Direction dir = Direction::None;
-      int count = 0;
-      while (pathNotClear && count < 20) {
-        // Register the starting position of this
-        // path segment: this is the main way to
-        // be sure that each segment is correctly
-        // saved. The path struct has a mechanism
-        // to make sure that we don't add twice
-        // the home coordinates: for the rest we
-        // assume that we won't try to register
-        // the same position more than once.
-        steps.push_back(s);
-        points.push_back(s);
-
-        std::cout << "-----" << std::endl;
-        std::cout << "Step " << count << std::endl;
-
-        std::vector<Point> iPoints;
-        bool obs = info.frustum->obstructed(s, xDir, yDir, d, iPoints, &obsP);
-
-        // If the path is obstructed, we need to
-        // march on the border of the obstacle
-        // until we walk around it. Note that we
-        // only consider an obstruction if the
-        // input argument says so.
-        bool obsWithinTarget = obs && (std::abs(obsP.x - p.x) < 1.0f && std::abs(obsP.y - p.y) < 1.0f);
-
-        // We want to process the potential
-        // intersection only if it is meaningful.
-        pathNotClear = (
-          obs // In case there is an obstruction
-          &&  // and
-          (
-            !obsWithinTarget // the obstruction does not
-                             // lie within the target
-            || // or (in case the obstruction is within
-               // the target)
-            !ignoreTargetObstruction // we aren't ignoring
-                                      // obstruction of the
-                                      // cell.
-          )
-        );
-
-        if (!pathNotClear) {
-          points.insert(points.end(), iPoints.cbegin(), iPoints.cend());
-          std::cout << "[PATH][" << segments.size() << "] start: " << s.x << "x" << s.y
-                    << " to " << p.x << "x" << p.y
-                    << " is unobstructed, registering "
-                    << s.x << "x" << s.y
-                    << std::endl;
-        }
-        else {
-          // Register the intermediate steps that were
-          // deemed valid by the exploration: this will
-          // get us as far as possible on this segment.
-          // To determine the last valid position we
-          // will get the second-to-last position in
-          // the `iPoints` vector: this should be the
-          // last valid position along the path.
-          // We can then start from there the search
-          // for a path around the obstacle.
-          Point pStop = s;
-
-          if (iPoints.size() > 2u) {
-            // Note that as we already pushed the
-            // steps related to the start position
-            // we will start at the second point
-            // reached by the obstruction detection
-            // process. This mean that we might not
-            // register anything.
-            for (unsigned id = 1u ; id < iPoints.size() - 1u ; ++id) {
-              points.push_back(iPoints[id]);
-            }
-
-            pStop = iPoints[iPoints.size() - 2u];
-            steps.push_back(pStop);
-          }
-
-          float xSave = s.x, ySave = s.y;
-
-          // Determine in which way we want to explore
-          // for unobstructed regions in the neighborhood
-          // of the path.
-          s = computeNextPosition(pStop, xDir, yDir, dir, *info.frustum);
-
-          std::cout << "[PATH][" << segments.size() << "] start: " << xSave << "x" << ySave
-                    << " to " << p.x << "x" << p.y
-                    << " is obstructed at "
-                    << obsP.x << "x" << obsP.y
-                    << ", (d: " << xDir << "x" << yDir << ")"
-                    << " stop: " << pStop.x << "x" << pStop.y
-                    << ", registering: " << xSave << "x" << ySave
-                    << " and " << pStop.x << "x" << pStop.y
-                    << ", starting from " << s.x << "x" << s.y
-                    << std::endl;
-
-          info.toDirection(s, p, xDir, yDir, d);
-        }
-
-        ++count;
-        std::cout << "-----" << std::endl;
+      if (!alg.findPath(steps)) {
+        return false;
       }
 
-      // Once a valid path is built, register it
-      // as the actual path.
+      // A path was found, register it.
       for (unsigned id = 0u ; id < steps.size() ; ++id) {
-        std::cout << "[PATH][END][" << segments.size() << "] registering "
-                  << steps[id].x << "x" << steps[id].y
-                  << std::endl;
         add(steps[id]);
       }
-      add(p);
 
-      cPoints.swap(points);
-
-      // A path could be generated.
       return true;
     }
 
